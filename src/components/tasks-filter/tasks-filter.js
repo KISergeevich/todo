1042d57afc6e3b743dd6b23594828c0a6f38e428
Kalinin Ivan <indimion57@gmail.com>
2023-10-24 18:25:01 +0300

    cahnge taskComp to func component

--- src/components/tasks-filter/tasks-filter.js
+++ src/components/tasks-filter/tasks-filter.js
@@ -1,58 +1,41 @@
 import './tasks-filter.css'
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-export default class TasksFilter extends Component {
-  onClick(filterValue) {
-    const { onChange } = this.props
-    onChange(filterValue)
-  }
-
-  clearComplete() {
-    const { clearComplete } = this.props
-    clearComplete()
-  }
-
-  render() {
-    const { filter } = this.props
-    return (
-      <>
-        <ul className="filters">
-          <li>
-            <button
-              type="button"
-              className={classNames({ selected: filter === 'All' })}
-              onClick={() => this.onClick('All')}
-            >
-              All
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              className={classNames({ selected: filter === 'Active' })}
-              onClick={() => this.onClick('Active')}
-            >
-              Active
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              className={classNames({ selected: filter === 'Completed' })}
-              onClick={() => this.onClick('Completed')}
-            >
-              Completed
-            </button>
-          </li>
-        </ul>{' '}
-        <button type="button" className="clear-completed" onClick={() => this.clearComplete()}>
-          Clear completed
-        </button>
-      </>
-    )
-  }
+export default function TasksFilter({ filter, clearComplete, onChange }) {
+  return (
+    <>
+      <ul className="filters">
+        <li>
+          <button type="button" className={classNames({ selected: filter === 'All' })} onClick={() => onChange('All')}>
+            All
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={classNames({ selected: filter === 'Active' })}
+            onClick={() => onChange('Active')}
+          >
+            Active
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={classNames({ selected: filter === 'Completed' })}
+            onClick={() => onChange('Completed')}
+          >
+            Completed
+          </button>
+        </li>
+      </ul>{' '}
+      <button type="button" className="clear-completed" onClick={() => clearComplete()}>
+        Clear completed
+      </button>
+    </>
+  )
 }
 
 TasksFilter.defaultProps = {
