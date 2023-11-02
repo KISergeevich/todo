21eef10ae486286cd68b0e82ff42be60220d249b
Kalinin Ivan <indimion57@gmail.com>
2023-11-02 14:50:59 +0300

    transport utils and hooks to folder src

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import filterFunction from '../utils/filter-function'
+import filterFunction from '../../utils/filter-function'
 import Header from '../header/header'
 import TaskList from '../task-list/task-list'
 import Footer from '../footer/footer'
 import './app.css'
-import useToDo from '../hooks/use-todo'
+import useToDo from '../../hooks/use-todo'
 
 export default function App() {
   const { listCount, filter, setFilter, pause, addNew, remove, complete, play, clear } = useToDo()
